6ba0582dbabb8a4cd657cfb21f6c1949fb008bd1
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-16 20:45:03 +0000

    Added asking for string and character

---
+++ ChangeString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a string!");
+
+            string phrase = Console.ReadLine();
+
+            Console.WriteLine("Give me a character!");
+
+            char character = char.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
