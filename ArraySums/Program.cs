4a4deae4204b3648f0f8ab4358740b5542e7ac79
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-17 09:30:38 +0000

    Added asking the user for the value of each coordinate

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -6,17 +6,46 @@ namespace ArraySums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qual o tamanho horizontal?");
-
+            // Ask user for horizontal size
+            Console.WriteLine("What's the horizontal size?");
             int horizontal_size = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Qual o tamanho vertical?");
-
+            // Ask user for vertical size
+            Console.WriteLine("What's the vertical size?");
             int vertical_size = int.Parse(Console.ReadLine());
 
-            float[,] matrix_reals = new float [horizontal_size, vertical_size];
+            // Initalize matrix of real numbers with the user's size
+            float[,] matrix_reals = new float [vertical_size, horizontal_size];
 
-            
+            // Print a new line on cmd
+            Console.WriteLine();
+
+            // Go through each column
+            for (int i = 0; i < matrix_reals.GetLength(0); i++)
+            {   
+                // Go through each line
+                for (int j = 0; j < matrix_reals.GetLength(1); j++)
+                {   
+                    Console.Write
+                    ($"What's the value for the ({i + 1}, {j + 1}) position?");
+
+                    matrix_reals[i, j] = float.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+
+            // Go through each column
+            for (int i = 0; i < matrix_reals.GetLength(0); i++)
+            {   
+                // Go through each line
+                for (int j = 0; j < matrix_reals.GetLength(1); j++)
+                {
+                    Console.Write(matrix_reals[i, j]);
+                    Console.Write(" ");
+
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
