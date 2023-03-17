6b17ccb49361e9199daea78575ac6ed27e38a9f0
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-17 09:19:44 +0000

    Added asking the user for the size of the matrix

---
+++ ArraySums/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArraySums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o tamanho horizontal?");
+
+            int horizontal_size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o tamanho vertical?");
+
+            int vertical_size = int.Parse(Console.ReadLine());
+
+            float[,] matrix_reals = new float [horizontal_size, vertical_size];
+
+            
+        }
+    }
+}
