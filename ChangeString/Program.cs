8af7b5daa4d5c80b01881f88241fa32661191446
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-17 12:45:02 +0000

    Added comments to change string program

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,21 +6,20 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
+            // Ask the user for a string
             Console.WriteLine("Give me a string!");
-
             string phrase = Console.ReadLine();
 
+            // Ask the user for a character
             Console.WriteLine("Give me a character!");
-
             char character = char.Parse(Console.ReadLine());
 
+            // For each character in the string, compared it with
+            // the user's character, if it's different from that character,
+            // print it
             foreach(char c in phrase)
-            {
                 if (c != character)
-                {
                     Console.Write(c);
-                }
-            }
         }
     }
 }
